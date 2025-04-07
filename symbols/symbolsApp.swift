c501edecb0afa3660fc575c20327d3f1aa7c9372
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-07 14:27:21 -0600

    Initial Commit

---
+++ symbols/symbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  symbolsApp.swift
+//  symbols
+//
+//  Created by Benjamin Rojo on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct symbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
