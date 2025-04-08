22bea70d41196bcb8414d2cae551e61f259e360c
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-08 16:06:41 -0600

    i add a URL image from the web

--- symbols/ContentView.swift
+++ symbols/ContentView.swift
@@ -36,6 +36,13 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .padding()
                 .background(Color.orange)
+            AsyncImage(url: URL(string: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AF17905AA41300F0921FAF50947D286E3C13FD4B058C5F0C48F2F4C25D73A5B4/scale?width=1200&aspectRatio=1.78&format=webp")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
         }
         .padding()
         .background(.blue)
