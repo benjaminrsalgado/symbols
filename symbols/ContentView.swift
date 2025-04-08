6114652c1bbe2d614296a3bb05321426cae9bdd1
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-08 16:23:13 -0600

    i add a backgorund in all.

--- symbols/ContentView.swift
+++ symbols/ContentView.swift
@@ -9,43 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "paintpalette")
-                .font(.system(size: 80))
-                .foregroundColor(.purple)
-            Image(systemName: "paintpalette")
-                .renderingMode(.original)
-                .font(.system(size: 80))
-            Image(systemName: "star")
-                .renderingMode(.original)
-                .font(.system(size: 80))
-            Image(systemName: "rainbow")
-                .renderingMode(.original)
-                .font(.system(size: 80))
-            Image(systemName: "star.fill")
-                .blendMode(.multiply)
-            Image(systemName: "star.fill")
-                .padding()
-                .background(Color.yellow)
-                .cornerRadius(189)
-            Image(systemName: "heart.fill")
-                .padding()
-                .background(Color.red)
-            Image(systemName: "star.fill")
-                .font(.system(size: 40))
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.orange)
-            AsyncImage(url: URL(string: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AF17905AA41300F0921FAF50947D286E3C13FD4B058C5F0C48F2F4C25D73A5B4/scale?width=1200&aspectRatio=1.78&format=webp")) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+
+            VStack {
+                Image(systemName: "paintpalette")
+                    .font(.system(size: 80))
+                    .foregroundColor(.purple)
+                
+                Image(systemName: "paintpalette")
+                    .renderingMode(.original)
+                    .font(.system(size: 80))
+
+                Image(systemName: "star")
+                    .renderingMode(.original)
+                    .font(.system(size: 80))
+
+                Image(systemName: "rainbow")
+                    .renderingMode(.original)
+                    .font(.system(size: 80))
+
+                Image(systemName: "star.fill")
+                    .blendMode(.multiply)
+
+                Image(systemName: "star.fill")
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(180)
+
+                Image(systemName: "heart.fill")
+                    .padding()
+                    .background(Color.red)
+
+                Image(systemName: "star.fill")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.orange)
+
+                AsyncImage(url: URL(string: "https://i8.amplience.net/i/naras/Taylor-Swift-Eras-Tour-Brazil-GettyImages-1801110030")) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 900, height: 230)
             }
+            .padding()
         }
-        .padding()
-        .background(.blue)
     }
 }
 
